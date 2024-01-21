c6e571ec34c24b6b5c408e25280e6953660f3cb9
Siva Sriram <sivasriram1206@gmail.com>
2024-01-21 11:22:10 +0530

    first commit

---
+++ src/components/Slider.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import { IoMdArrowDropright,IoMdArrowDropleft } from "react-icons/io";
+
+function Slider(props) {
+    const {url}=props
+    const length =url.length-1
+    const [index,setIndex]=useState(0)
+    const handlePrev=()=>{
+        index==0?setIndex(length):setIndex(index-1)
+    }
+    const handleNext=()=>{
+        index==length?setIndex(0):setIndex(index+1)
+    }
+  return (
+    <div className='main'>
+        <div className='imgs'>
+            <IoMdArrowDropleft className='icon' onClick={handlePrev}/>
+            <img src={url[index]} alt='siva'/>
+            <IoMdArrowDropright className='icon' onClick={handleNext}/>
+        </div>
+    <div className='bottombar'>
+        {url.map((url,i)=>(<div onClick={()=>
+     {setIndex(i)}} key={i} className={`bar ${index==i?'barWhite':''}`}>_</div>
+     ))}
+    </div>
+    </div>
+  )
+}
+
+export default Slider
